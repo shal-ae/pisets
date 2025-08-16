0393513f3907e3519a60ab74f9d2095febf392d6
Alex Shubin <Alex.Shubin@gmail.com>
2025-08-16 14:25:02 +0300

    ng 20, tailwind

--- apps/front/tailwind.config.js
+++ apps/front/tailwind.config.js
@@ -4,8 +4,8 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'apps/front/src/**/!(*.stories|*.spec).{ts,html}'),
-    // ...createGlobPatternsForDependencies(__dirname),
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
