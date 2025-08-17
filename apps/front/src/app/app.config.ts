3115ab6aa79efaf1557cff49a747212c4d173a5a
Alex Shubin <Alex.Shubin@gmail.com>
2025-08-17 13:39:18 +0300

    ngZorro config

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -1,17 +1,28 @@
-import { registerLocaleData } from '@angular/common'
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http'
-import ru from '@angular/common/locales/ru'
-import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, Provider } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { provideClientHydration } from '@angular/platform-browser'
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { provideRouter, withComponentInputBinding } from '@angular/router'
-import { provideNzI18n, ru_RU } from 'ng-zorro-antd/i18n'
-import { routes } from './app.routes'
-import { AuthInterceptor } from './shared/interceptors/auth.interceptor'
-import { ConfigService } from './shared/services/config.service'
+import { registerLocaleData } from '@angular/common';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import ru from '@angular/common/locales/ru';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+  Provider,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideNzI18n, ru_RU } from 'ng-zorro-antd/i18n';
+import { routes } from './app.routes';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+import { ConfigService } from './shared/services/config.service';
+import { provideNzConfig } from 'ng-zorro-antd/core/config';
+import { ngZorroConfig } from './ngZorro.config';
 
-registerLocaleData( ru )
+registerLocaleData(ru);
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
@@ -25,19 +36,20 @@ const appInitializerFn = ( configService: ConfigService ) => {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter( routes, withComponentInputBinding() ),
-    provideNzI18n( ru_RU ),
-    importProvidersFrom( FormsModule ),
+    provideRouter(routes, withComponentInputBinding()),
+    provideNzI18n(ru_RU),
+    importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
-    provideHttpClient( withInterceptorsFromDi(), withFetch() ),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     INTERCEPTOR_PROVIDER,
     // provideClientHydration(),
+    provideNzConfig(ngZorroConfig),
     ConfigService,
     {
       provide: APP_INITIALIZER,
       useFactory: appInitializerFn,
       multi: true,
-      deps: [ ConfigService ],
+      deps: [ConfigService],
     },
   ],
-}
+};
---
+++ apps/front/src/app/ngZorro.config.ts
@@ -0,0 +1,7 @@
+import { NzConfig } from 'ng-zorro-antd/core/config';
+
+export const ngZorroConfig: NzConfig = {
+  theme: {
+    primaryColor: '#182fff'
+  }
+};
